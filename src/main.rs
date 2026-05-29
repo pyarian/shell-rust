aa9a227e81d87da123253ae49805f7e7449cd857
rishi <aryan.singh24b@iiitg.ac.in>
2026-05-29 20:23:28 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
@@ -6,32 +5,29 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        //wait for user input
-
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
 
-        //checking exit
-
         if command == "exit" {
             break;
         }
-        let c = command.split_whitespace().nth(1).unwrap();
 
-        if command.starts_with("echo") {
-            println!("{}", command[5..]);
-        } else if command.starts_with("type") {
+        if command.starts_with("echo ") {
+            println!("{}", &command[5..]);
+        } else if command.starts_with("type ") {
+            let c = command.split_whitespace().nth(1).unwrap();
+
             match c {
                 "echo" | "exit" | "type" => {
                     println!("{} is a shell builtin", c);
                 }
                 _ => {
-                    println!("{}: not found ", c.trim());
+                    println!("{}: not found", c);
                 }
             }
         } else {
-            println!("{} not found", command[5..]);
+            println!("{}: not found", command);
         }
     }
 }
