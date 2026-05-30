fc3c93f62adbaf44a39309e012e45b6278074ab3
rishi <aryan.singh24b@iiitg.ac.in>
2026-05-30 11:26:35 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::{self, Write};
+use std::{
+    fs::Metadata,
+    io::{self, Write},
+    os::unix::fs::PermissionsExt,
+};
 
 fn main() {
     loop {
@@ -16,14 +20,36 @@ fn main() {
         if command.starts_with("echo ") {
             println!("{}", &command[5..]);
         } else if command.starts_with("type ") {
-            let c = command.split_whitespace().nth(1).unwrap();
+            let cmd = command.split_whitespace().nth(1).unwrap();
 
-            match c {
+            match cmd {
                 "echo" | "exit" | "type" => {
-                    println!("{} is a shell builtin", c);
+                    println!("{} is a shell builtin", cmd);
                 }
                 _ => {
-                    println!("{}: not found", c);
+                    println!("{}: not found", cmd);
+                }
+            }
+
+            let path = match std::env::var("PATH") {
+                Ok(p) => p,
+                Err(_) => {
+                    println!("PATH not found");
+                    return;
+                }
+            }
+            .split(':');
+
+            for dir in path {
+                let full_path = Path::new(dir).join(cmd);
+
+                if full_path.exists() {
+                    let metadata = std::fs::metadata(&full_path).unwrap();
+                    let permissions = metadata.permissions();
+                    if metadata.permissions().mode() & 0o111 != 0 {
+                        println!("{} is {}", cmd, full_path.display());
+                        break;
+                    }
                 }
             }
         } else {
